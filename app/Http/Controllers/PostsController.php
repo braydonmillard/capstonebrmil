6a2be61f50f72c3ec9a12bfc46f12afe824da5ad
Your Name <you@example.com>
2021-11-04 13:37:34 -0400

    idk

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -52,7 +52,7 @@ class PostsController extends Controller
 
         //$image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
 
-        Image::make($image->getRealPath())->fit(1200, 1200)->save( "/storage/{$image}");
+        Image::make($image->getRealPath())->fit(1200, 1200)->save( "/storage{$image}");
         //$image->save();
 
         auth()->user()->posts()->create([
