c0213a56227fcd0f3c23b9f4144149dce222685f
Your Name <you@example.com>
2021-11-10 14:58:06 -0500

    Navbar edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,7 +74,7 @@
                                         @csrf
                                     </form>
 
-                                    <a class="dropdown-item" href="/profile/{{ $post->user->id }}">
+                                    <a class="dropdown-item" href="/profile/{{ Auth::user()->id }}">
                                         {{ __('Go to my profile') }}
                                     </a>
                                 </div>
