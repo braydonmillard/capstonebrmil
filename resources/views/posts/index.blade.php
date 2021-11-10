a76c22b76f94bc5431e6683ec7b3df6826c09699
Your Name <you@example.com>
2021-11-10 14:24:53 -0500

    Featured recipes on home page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -28,6 +28,33 @@
         </div>
     @endforeach
 -->
+    <h4>Featured recipes:</h4>
+    @foreach($posts as $post WHERE $post->is_featured = 1)
+        <div class="row">
+            <div class="col-6 offset-3">
+                <a href="/show/{{ $post->id }}">
+                    <img src="https://brmil.s3.us-east-2.amazonaws.com/{{ $post->image }}" class="w-75">
+                </a>
+            </div>
+        </div>
+        <div class="row pt-2 pb-4">
+            <div class="col-6 offset-3">
+                <div>
+                    <p>
+                    <span class="font-weight-bold">
+                    {{ $post->title }}
+                    </span> <br>
+                    <i>{{ $post->caption }}</i> <br>
+                    By  <a href="/profile/{{ $post->user->id }}">
+                            <span class="text-dark">{{ $post->user->username }}</span>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    @endforeach
+
+
     <h4>New from all users:</h4>
     @foreach($posts as $post)
         <div class="row">
