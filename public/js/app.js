b26a6a15926b936cecd856522b35b641db542b03
Your Name <you@example.com>
2021-11-18 15:47:29 -0500

    idk

--- public/js/app.js
+++ public/js/app.js
@@ -2077,6 +2077,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  name: 'favourite',
   props: ['post', 'favourited'],
   data: function data() {
     return {
