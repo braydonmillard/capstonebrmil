aa710706a7ddb112bb5e8d7303d838df6490278a
Your Name <you@example.com>
2021-12-03 19:18:07 -0500

    Cleanup, pgsql syntax compliance

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -20,15 +20,9 @@ class PostsController extends Controller
 {
     public function __construct(){
 
-        //$this->middleware('auth');
     }
 
     public function index(){
-        //$users = auth()->user()->following()->pluck('profiles.user_id');
-
-        //$users = auth()->user()->following()->pluck('profiles.user_id');
-
-        //$posts = Post::whereIn('user_id', $users)->latest()->limit(3)->get();
 
         $posts = Post::latest()->limit(3)->get();
 
@@ -51,7 +45,6 @@ class PostsController extends Controller
                             ->orderBy('total', 'desc')
                             ->get();
 
-        //return view('posts.index', compact('posts', 'allposts'));
         return view('posts.index', compact('posts', 'searchQueries', 'queriesGrouped', 'usersGrouped', 'postsGrouped'));
     }
 
@@ -75,47 +68,12 @@ class PostsController extends Controller
             'image' => ['required', 'image'],
         ]);
 
-        //dd(config('filesystems.disks.s3.region'));
-        
-        //$imagePath = request('image')->store('uploads', 's3');
-
         $imagePath = request('image');
 
         $filePath = 'images/' . $imagePath->getClientOriginalName();
 
         
         Storage::disk('s3')->put($filePath, file_get_contents($imagePath), 'public');
-
-
-        //Wow it seems to work
-
-
-        //$contents = Storage::disk('s3')->get($filePath);        
-        //dd($contents);
-        //upload to s3 working but opening does not
-        
-        //dd(storage_path("{$imagePath}"));
-
-        //Storage::disk('s3')->setVisibility($imagePath, 'public');
-        
-        //dd($idk);
-
-        //dd($imagePath);
-        
-        //Storage::disk('s3')->put($imagePath, file_get_contents($imagePath));
-
-/*
-        $image = Image::make([
-            'filename' => basename($imagePath),
-            'url' => Storage::disk('s3')->url($imagePath)
-        ]);*/
-
-        //dd(public_path("storage/{$imagePath}"));
-
-        //$image = Image::make(public_path("storage/{$imagePath}"));
-        //$image = Image::make($request->file('photo')->getRealPath());
-
-        //$image->save();
 
         auth()->user()->posts()->create([
             'caption' => $data['caption'],
@@ -131,52 +89,26 @@ class PostsController extends Controller
     public function search(Request $request){
         $search_text = $_GET['query'];
 
-        /*$query = Post::query();
-
-        if ($s = $request->input('s')) {
-            $query->whereRaw("title LIKE '%'. $s .'%' ");
-        }*/
         SearchQuery::create([ 'query_text' => $search_text,
         ]);
 
-
-
         if ($request->sort == "Newest")
         {
-            $posts = Post::where('title', 'LIKE', '%'.$search_text.'%')->orderBy('id', 'desc')->get();
+            $posts = Post::where('title', 'ILIKE', '%'.$search_text.'%')->orderBy('id', 'desc')->get();
         }
         else if($request->sort == "Most_Popular")
         {
-            $posts = Post::where('title', 'LIKE', '%'.$search_text.'%')->orderBy('times_made', 'desc')->get();
+            $posts = Post::where('title', 'ILIKE', '%'.$search_text.'%')->orderBy('times_made', 'desc')->get();
         }
         else
         {
-        $posts = Post::where('title', 'LIKE', '%'.$search_text.'%')->get();
+        $posts = Post::where('title', 'ILIKE', '%'.$search_text.'%')->get();
         }
 
-
-
-        //if ($request->sort == "post_latest"){
-        //    $posts->orderBy('id', 'desc');
-        //}
-
-
-        //$posts = $posts->get();
-
-        //dd($posts);
-
-        //dd($request->sort);
-            /*
-        if ($sort = $request->input('sort')) {
-            $query->orderBy('id', 'desc', $sort);
-        }*/
-
         return view('posts.search',compact('posts', 'search_text'));
-        //return $query->get();
     }
 
     public function sort(){
-        //$search_text = $searchText;
 
         $posts = Post::latest()->get();
 
@@ -184,61 +116,30 @@ class PostsController extends Controller
     }
 
     public function rate(Post $post, Request $request){
-        //$search_text = $searchText;
 
         //If user gives invalid rating value, ignore and return to page
         if($request->rating > 5 || $request->rating < 1){
             return back();
         }
-
-        //dd(auth()->user()->id);
-
-        /*
-            Just divide sum of ratings by count
-        */
         
         $ratings = $post->Rating;
-
-        //dd($ratings);
-
-        //$ratings->avg
-
-        //dd($post->Rating->first());
-        //dd(contains($user->id)
-        //round(avgRating, 1);
-        
         
         //User can only rate post once
         if($ratings->contains('user_id', auth()->user()->id)){
             return view('posts.show', compact('post'));
         }
 
-        //dd(Rating::all());
-        
-        //if(Post::find($post->id))
-
-        //dd($request->rating);
-
         $myRating = Rating::create([ 
         'post_id' => $post->id,
         'rating' => $request->rating,
         'user_id' => auth()->user()->id,
         ]);
-        //$posts = Post::latest()->get();
-
-        //$avgRating = round(Rating::avg('rating'), 1);
-
-        //dd($avgRating);
 
         $ratingArray = $post->rating->pluck('rating');
         
-        //dd(count($ratingArray->all()));
-
-        //dd(array_sum($ratingArray->all()));
 
         $avgRating = array_sum($ratingArray->all()) / count($ratingArray->all());
 
-        //dd(round($avgRating, 1));
 
         $avgRating = round($avgRating, 1);
 
@@ -246,29 +147,11 @@ class PostsController extends Controller
 
         $post->update(['averaged_rating' => $avgRating]);
 
-        //Set average rating field for post
-
-        //$myPost->rating
-
-        //$myPost->rating()->attach($myRating);
-
-        //dd($myPost->rating);
-
         return view('posts.show', compact('post'));
-        //return Post::find($post->id);
-        //return back();
-        //return redirect()->route('posts');
     }
 
-    public function feature(Post $post){
-        //$search_text = $_GET['query'];
-
-        //$posts = Post::where('title', 'LIKE', '%'.$search_text.'%')->get();
-
-        //$post->is_featured=1;
-
-        //return view('posts.show', compact('post'));
-
+    public function feature(Post $post)
+    {
         $myPost = Post::find($post->id);
 
         $myPost->update(['is_featured' => 1]);
@@ -338,7 +221,6 @@ class PostsController extends Controller
 
         $review->update(['thumbs_up' => $times_liked]);
         
-        
         return back();
     }
 
@@ -365,12 +247,6 @@ class PostsController extends Controller
 
         if (request('image')) {
             
-            /*$imagePath = request('image')->store('profile', 'public');
-
-            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
-            $image->save();*/
-
-            
         $imagePath = request('image');
 
         $filePath = 'images/' . $imagePath->getClientOriginalName();
@@ -396,7 +272,6 @@ class PostsController extends Controller
         if(auth()->user()->id == $post->user_id){
             $post->delete();
         }
-
 
         return redirect("/");
     }
